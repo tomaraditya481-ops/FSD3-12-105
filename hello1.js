e3b5901440a005537e4a22a6ac1c21690a60152e
Aditya Tomar <tomaraditya481@gmail.com>
2026-07-23 12:01:45 +0530

    intitial commit

---
+++ hello1.js
@@ -0,0 +1,17 @@
+console.log("aditya");
+
+
+//their sum
+
+function sum(a, b) {
+    return a + b;
+}
+
+const add = (a,b) => {
+    return a + b;
+};
+const add2 = (a, b) => a + b;
+
+console.log(sum(20,10));
+console.log(add(20,10));
+console.log(add2(20,10));
